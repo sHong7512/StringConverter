022b93f3146e1e3ff0182f26c83e50b84435ae57
SoonHong Kwon <ksh7512@uangel.com>
2022-04-20 10:49:49 +0900

    [Add] HyperLink

--- app/src/main/java/com/shong/stringconverter/MainActivity.kt
+++ app/src/main/java/com/shong/stringconverter/MainActivity.kt
@@ -12,15 +12,44 @@ class MainActivity : AppCompatActivity() {
         val stringConverter = StringConverter(this)
         val textView = findViewById<TextView>(R.id.textView)
         textView.setText(stringConverter.colorChange(textView.text.toString(), "sHong", "#00FF00"))
+
         val textView2 = findViewById<TextView>(R.id.textView2)
         textView2.setText(stringConverter.fontChange(textView2.text.toString(), "sHong"))
 
-//        val sortedKeyList = rawMap.keys.sortedWith(Comparator(StringSortation::compare))
+        val txt = "- 아이유 : https://www.melon.com/artist/timeline.htm?artistId=261143   " +
+                "- 블랙핑크 : https://www.melon.com/artist/timeline.htm?artistId=995169"
+        findViewById<TextView>(R.id.hyperlinkTextView).text = stringConverter.urlHyperLink(txt)
+    }
+
+    //기획안대로 정렬
+//    fun sortForProposal(contactList: MutableList<Contact>): MutableList<Contact>{
+//        //rep 분류
+//        val erasedContactList = mutableListOf<Contact>()
+//        for (c in contactList) {
+//            if (c.mobile?.isNotEmpty() == true){
+//                c.phoneRepType = 0
+//                erasedContactList.add(c)
+//            } else if (c.work?.isNotEmpty() == true){
+//                c.phoneRepType = 1
+//                erasedContactList.add(c)
+//            } else if (c.home?.isNotEmpty() == true){
+//                c.phoneRepType = 2
+//                erasedContactList.add(c)
+//            }
+//        }
+//
+//        //정렬
+//        val rawMap = mutableMapOf<String, MutableList<Contact>>()
+//        for (c in erasedContactList) {
+//            val iw = wordOperator.getInitial(c.name).initalWord
+//            rawMap[iw]?.add(c) ?: run { rawMap[iw] = mutableListOf(c) }
+//        }
+//        val sortedKeyList = rawMap.keys.sortedWith(Comparator(stringSortation::compare))
 //        val sortedContactList = mutableListOf<Contact>()
 //        for (key in sortedKeyList) {
-//            sortedContactList += rawMap[key]?.sortedWith(Comparator(StringSortation::compareContact))
+//            sortedContactList += rawMap[key]?.sortedWith(Comparator(stringSortation::compareContact))
 //                ?: continue
 //        }
-
-    }
+//        return sortedContactList
+//    }
 }
--- app/src/main/java/com/shong/stringconverter/StringConverter.kt
+++ app/src/main/java/com/shong/stringconverter/StringConverter.kt
@@ -11,6 +11,11 @@ import android.text.style.TypefaceSpan
 import android.util.Log
 import android.widget.EditText
 import androidx.core.content.res.ResourcesCompat.getFont
+import java.net.MalformedURLException
+import java.net.URISyntaxException
+import java.net.URL
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class StringConverter(val context: Context) {
     val TAG = this::class.java.simpleName + "_sHong"
@@ -186,6 +191,65 @@ class StringConverter(val context: Context) {
             "%수신자명%",
             colorString
         )
+    }
+
+    fun isUrl(text: String?): Boolean {
+        val p: Pattern = Pattern.compile("^(?:https?:\\/\\/)?(?:www\\.)?[a-zA-Z0-9./]+$")
+        val m: Matcher = p.matcher(text)
+        if (m.matches()) return true
+        var u: URL? = null
+        try {
+            u = URL(text)
+        } catch (e: MalformedURLException) {
+            Log.e(TAG, "$e")
+            return false
+        }
+        try {
+            u.toURI()
+        } catch (e: URISyntaxException) {
+            Log.e(TAG, "$e")
+            return false
+        }
+        return true
+    }
+
+    fun urlHyperLink(_text: String): String {
+        var text = _text
+        var tmpText = text //원본을 저장
+        var start = text.indexOf("http") //http문자열 위치
+        var space = 0 //공백 위치
+        var bodyText = ""
+        var tailText = ""
+        var check = false
+        val urlList = ArrayList<String>()
+        if (start >= 0) { //http가 존재한다면 true를 입력
+            check = true
+        }
+        while (check) { //http가 존재한다면 실행
+            bodyText = text.substring(start) //http부터 문자열 자름
+            space = bodyText.indexOf(" ") //자른 문자열에서 공백위치 확인
+            if (space > 0) { //공백이 존재한다면
+                tailText = bodyText.substring(space) //공백 뒤 문자열 저장
+                bodyText = bodyText.substring(0, space) //http 주소부분만 잘라서 tailText 부분에 입력
+            } else {
+                tailText = "" //공백이 없으니 뒤 문자열이 없음
+            }
+            urlList.add(bodyText) //http 주소를 ArrayList에 저장
+            start = tailText.indexOf("http") //뒤에 http 주소가 존재하는지 확인
+            if (start >= 0) {
+                check = true
+                text = tailText //존재한다면 뒷부분을 text에 입력
+            } else {
+                check = false
+            }
+        }
+        for (url in urlList) { //ArrayList에 넣었던 http주소를 for문으로 실행
+            //tmpText 문자열에서 url부분 찾아서 머리+몸통(href추가)+꼬리 만들어서 href가 삽입된 문자열 완성
+            tmpText = (tmpText.substring(0, tmpText.indexOf(url)) //머리
+                    + "<a href=\"" + url + "\" target=\"_blank\">" + url + "</a>" //몸통
+                    + tmpText.substring(tmpText.indexOf(url) + url.length)) //꼬리
+        }
+        return tmpText
     }
 
 }
